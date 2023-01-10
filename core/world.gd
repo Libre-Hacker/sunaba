8e26e4ac7f28f1c1e2efa3f76ecf2e4b169c6b20
mintkat <lucasbear9@gmail.com>
2023-01-09 16:27:52 -0800

    Some cleaning up

---
+++ core/world.gd
@@ -0,0 +1,42 @@
+extends Spatial
+
+export var main_node_path : NodePath
+
+var map = null
+
+var prop_num : int = 1
+var prop_name : String
+
+var can_rebuild = false
+
+var mouse_over_ui = false
+
+onready var qodot_map = $QodotMap
+onready var main_node = get_node(main_node_path)
+onready var http_request = $HTTPRequest
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	http_request.use_threads = true
+	QodotDependencies.check_dependencies(http_request)
+
+
+func import_map(path):
+	var file = File.new()
+	file.open(path, File.READ)
+	var sbg_text = file.get_as_text()
+	map = str2var(sbg_text)
+	file.close()
+
+func load_map(path):
+	if path != null:
+		qodot_map.set_map_file(path)
+		qodot_map.verify_and_build()
+		qodot_map.unwrap_uv2()
+
+func on_mouse_entered():
+	mouse_over_ui = true
+
+
+func on_mouse_exited():
+	mouse_over_ui = false

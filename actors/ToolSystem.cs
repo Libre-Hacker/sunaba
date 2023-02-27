dc12db8ca83a8bf9f98cae1dd534fa5820a58643
mintkat <85989407+m1ntkat@users.noreply.github.com>
2023-02-26 17:03:00 -0900

    Added Compoments

--- actors/ToolSystem.cs
+++ actors/ToolSystem.cs
@@ -12,5 +12,40 @@ namespace Sunaba.Actors
         public String tool2;
         public String tool3;
         public String tool4;
+
+        public override void _Input(InputEvent @event)
+        {
+            if (Input.IsActionJustPressed("interact"))
+            {
+                if (toolToSpawn != null)
+                {
+                    //
+                }
+            }
+        }
+
+        public void addTool(String tool)
+        {
+            if (tool1 == null)
+            {
+                tool1 = tool;
+                currentTool = 1;
+            }
+            else if (tool2 == null)
+            {
+                tool2 = tool;
+                currentTool = 2;
+            }
+            else if (tool3 == null)
+            {
+                tool3 = tool;
+                currentTool = 3;
+            }
+            else if (tool4 == null)
+            {
+                tool4 = tool;
+                currentTool = 4;
+            }
+        }
     }
 }

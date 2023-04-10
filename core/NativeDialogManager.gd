95e3529340b5f073b994088d34dc5029145575e0
mintkat <85989407+m1ntkat@users.noreply.github.com>
2023-04-10 05:39:44 -0800

    Create NativeDialogManager.gd

---
+++ core/NativeDialogManager.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$NativeAcceptDialog1.show()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func show_native_file_dialog():
+	$NativeFileDialog.show()
+
+
+func _on_native_file_dialog_file_selected(path):
+	get_parent().OnFileSelected(path)

03a66abfd5087cc1df5421c760802560ac524580
mintkat <85989407+m1ntkat@users.noreply.github.com>
2023-04-10 05:55:50 -0800

    Added NativeNotification1

--- core/NativeDialogManager.gd
+++ core/NativeDialogManager.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$NativeAcceptDialog1.show()
+	$NativeNotification1.send()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

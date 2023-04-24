fd0c1fc7c850886529cb710a931281cfbd9de437
mintkat <lucasbear9@gmail.com>
2023-04-23 19:28:16 -0800

    Update buildall.py

--- buildall.py
+++ buildall.py
@@ -9,11 +9,19 @@ if len(sys.argv) == 2:
 build_linux = os.system("python ./build.py linux zip " + godot_path)
 
 if build_linux == 0:
-    build_win32 = os.system("python ./build.py win32 nsis " + godot_path)
+    build_mac = os.system("python ./build.py mac " + godot_path)
     
-    if build_win32 == 0:
-        exit()
+    if build_mac == 0:
+        build_win32 = os.system("python ./build.py win32 nsis " + godot_path)
+    
+        if build_win32 == 0:
+            exit()
+        else:
+            exit(1)
     else:
         exit(1)
+    
+    
+    
 else:
     exit(1)

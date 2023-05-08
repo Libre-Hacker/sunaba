d4e3ff4b92a6ee3a030779bb836b88560c4cb7f9
mintkat <lucasbear9@gmail.com>
2023-05-08 09:19:05 -0800

    Added tmp folder, Updated Lua Scripts

--- Scripts/npcTest1.lua
+++ Scripts/npcTest1.lua
@@ -1,11 +1,23 @@
-function start()
-    print('Hello World! Im an NPC')
+-- Define a Lua class
+MyNPC = {}
+
+-- Define a constructor for the class
+function MyNPC.new()
+  local obj = {}
+  setmetatable(obj, MyNPC)
+  MyNPC.__index = MyNPC
+  return obj
 end
 
-function update()
-
+-- Define a method for the class
+function MyNPC:start()
+    Chat::print('Hello World! Im an NPC')
 end
 
-function physicsUpdate()
-    
+function MyNPC:update()
+    -- Do something
+end
+
+function MyNPC:physicsUpdate()
+    -- Do something
 end
---
+++ setup.py
@@ -0,0 +1,7 @@
+import xml.etree.ElementTree as ET
+
+godot_version = "4.0.2"
+
+godot_bin = "https://github.com/godotengine/godot/releases/download/" + godot_version + "-stable/Godot_v" + godot_version + "-stable_mono_linux_x86_64.zip"
+
+temp_folder = "./tmp"

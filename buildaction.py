d98da0271194e177c2b491494c3d15a65e94bc35
mintkat <lucasbear9@gmail.com>
2023-05-08 11:14:23 -0800

    Added buildaction.py

---
+++ buildaction.py
@@ -0,0 +1,12 @@
+import platform
+import os
+
+print(platform.system())
+
+if platform.system() == "Linux":
+    os.system("python ./build.py linux zip")
+elif platform.system() == "Darwin":
+    os.system("python ./build.py mac")
+elif platform.system() == "Windows":
+    os.system("python ./build.py win32 nsis")
+

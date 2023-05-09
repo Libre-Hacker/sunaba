a635d28f150f5396417cc4a8bd7d7e97d761b155
mintkat <lucasbear9@gmail.com>
2023-05-09 08:25:23 -0800

    Update lua script

--- Scripts/John.lua
+++ Scripts/John.lua
@@ -1,17 +1,9 @@
 -- Define a Lua class
 John = {}
 
--- Define a constructor for the class
-function John.new()
-  local obj = {}
-  setmetatable(obj, John)
-  John.__index = John
-  return obj
-end
-
 -- Define a method for the class
 function John:start()
-    Chat:print(John:name 'Hello World! Im an NPC')
+    Chat.print(John:name 'Hello World! Im an NPC')
 end
 
 function John:update()

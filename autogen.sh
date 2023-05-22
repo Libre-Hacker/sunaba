2225b4984b20d53021d28796010794942b654689
mintkat <lucasbear9@gmail.com>
2023-05-22 10:38:10 -0800

    updated autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -14,3 +14,15 @@ cd "$ORIGDIR"
 if test -z "$NOCONFIGURE"; then
     "$srcdir"/configure "$@"
 fi
+
+if [ ! -d ./bin ]; then
+  mkdir -p ./bin;
+fi
+
+if [ ! -d ./bin/linux ]; then
+  mkdir -p ./bin/linux;
+fi
+
+if [ ! -d ./binWin32 ]; then
+  mkdir -p ./binWin32;
+fi

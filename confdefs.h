d270f4ac4d85ce6eeea73568ebf0a161917836bc
mintkat <lucasbear9@gmail.com>
2023-06-04 16:47:12 -0800

    Updated Version Number

--- confdefs.h
+++ confdefs.h
@@ -1,9 +1,9 @@
 /* confdefs.h */
 #define PACKAGE_NAME "sunaba"
 #define PACKAGE_TARNAME "sunaba"
-#define PACKAGE_VERSION "0.4.5"
-#define PACKAGE_STRING "sunaba 0.4.5"
+#define PACKAGE_VERSION "0.5.0"
+#define PACKAGE_STRING "sunaba 0.5.0"
 #define PACKAGE_BUGREPORT "sunabagame@gmail.com"
 #define PACKAGE_URL ""
 #define PACKAGE "sunaba"
-#define VERSION "0.4.5"
+#define VERSION "0.5.0"
